7a188075f81e1fc1474cd96365176b43c4cdd7aa
kailash-unc <kailash@email.unc.edu>
2023-01-01 21:10:27 -0500

    Adding local files to repo

---
+++ index.js
@@ -0,0 +1,51 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+
+app.get("/api/:date", function(req, res) {
+  var inputdate = req.params.date
+  if (/\d{5,}/.test(inputdate)) { inputdate = parseInt(inputdate) };
+  var date = new Date(inputdate);
+  if (date.toUTCString() == "Invalid Date") {
+    res.json({ error: date.toUTCString() })
+  }
+  else{
+  res.json({ unix: date.valueOf(), utc: date.toUTCString() })
+  }
+})
+
+app.get("/api", function(req, res) {
+  var date = new Date();
+  res.json({ unix: date.valueOf(), utc: date.toUTCString() })
+  
+})
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
